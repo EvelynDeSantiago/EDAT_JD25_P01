a49649d70689b5158122a97bacf1f3076207ca15
evelyn <evely@EVELYN-DSC>
2025-08-28 11:04:24 -0600

    Version 02: Funciones Lambda

---
+++ UI.NumerosAleatorios/frmDatosAleatorios.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace UI.NumerosAleatorios
+{
+    partial class frmDatosAleatorios
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMostrarMensaje = new Button();
+            SuspendLayout();
+            // 
+            // btnMostrarMensaje
+            // 
+            btnMostrarMensaje.Location = new Point(394, 236);
+            btnMostrarMensaje.Name = "btnMostrarMensaje";
+            btnMostrarMensaje.Size = new Size(180, 29);
+            btnMostrarMensaje.TabIndex = 0;
+            btnMostrarMensaje.Text = "Mostrar Mensaje";
+            btnMostrarMensaje.UseVisualStyleBackColor = true;
+            btnMostrarMensaje.Click += btnMostrarMensaje_Click;
+            // 
+            // frmDatosAleatorios
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnMostrarMensaje);
+            Name = "frmDatosAleatorios";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMostrarMensaje;
+    }
+}
